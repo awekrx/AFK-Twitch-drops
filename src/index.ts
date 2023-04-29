ccf97d6833f60201bf7eb305eeaa3d977187e868
Alexey Klygin <iosdevfarconsoft@gmail.com>
2023-04-29 22:05:03 +0200

    Anon proxy to different file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import proxyChain from 'proxy-chain'
+import getAnonProxy from './anonProxy.js'
 import logging from 'improved-logging'
 import Bot from './Bot.js'
 import env from './env.js'
@@ -11,16 +11,8 @@ const smallestArray = Math.min(tokens.length, proxies.length + 1) // +1 because 
 logging.important(`Starting ${smallestArray} bots...`)
 
 for (let i = 0; i < smallestArray; i++) {
-    const proxy = await getAnonProxy(i)
+    const proxy = await getAnonProxy(proxies, i)
     await new Bot(tokens[i], proxy).start()
-}
-
-async function getAnonProxy(i: number): Promise<string | undefined> {
-    if (i === 0) return undefined
-    const proxy = proxies[i - 1]
-    const anonProxy = await proxyChain.anonymizeProxy(proxy)
-    logging.info(`Anonymizing proxy ${proxy} to ${anonProxy}`)
-    return anonProxy
 }
 
 logging.success('All bots started!')
