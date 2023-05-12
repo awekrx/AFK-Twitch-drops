954f4bc4d056115b07a76fc52389daca4733a1ae
Alexey Klygin <iosdevfarconsoft@gmail.com>
2023-05-12 19:56:18 +0200

    Disabled cookie usage

--- src/Bot.ts
+++ src/Bot.ts
@@ -23,10 +23,10 @@ export default class Bot extends BotLogic {
         // Start bot
         logging.info("Starting bot... For streamer: " + config.streamer);
         await this.startWatching();
-        logging.info("Setting username...");
-        await this.setUsername(this.streamPage);
+        // logging.info("Setting username...");
+        // await this.setUsername(this.streamPage);
 
-        this.checkUsername();
+        // this.checkUsername();
 
         if (config.adultcontent) {
             try {
@@ -130,7 +130,7 @@ export default class Bot extends BotLogic {
         this.browser = await puppeteer.launch(this.browserConfig);
         this.streamPage = await this.browser.newPage();
         await this.streamPage.setUserAgent(config.userAgent);
-        await this.streamPage.setCookie(this.cookie);
+        // await this.streamPage.setCookie(this.cookie);
         this.browser.on("disconnected", () => {
             logging.error(`Browser closed, hope u wanted it`);
             // process.exit(1);
