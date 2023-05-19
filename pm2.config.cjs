6e8decf74dbb226dbb063c22bdfaf378d7fad248
Alexey Klygin <iosdevfarconsoft@gmail.com>
2023-05-19 04:17:13 +0200

    pm2 ressurect

---
+++ pm2.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [{
+        name: "Twitch Bot viewer",
+        script: "dist/index.js",
+        exec_mode: "cluster",
+        out: "log/pm2.log",
+        node_args: "--experimental-json-modules"
+    }]
+};
