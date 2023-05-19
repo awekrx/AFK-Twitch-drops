8b2bb408a3aca289f1c17f59f48c7a947e1aac24
Alexey Klygin <iosdevfarconsoft@gmail.com>
2023-05-19 04:25:50 +0200

    Update pm2.config.cjs

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "Twitch Bot viewer",
-        script: "dist/index.js",
+        script: "./dist/index.js",
         exec_mode: "cluster",
         out: "log/pm2.log",
         node_args: "--experimental-json-modules"
